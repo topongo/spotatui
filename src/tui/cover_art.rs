17933ec4e612e22f83cdd2380260a4fc9deb1d14
Lorenzo Bodini <lorenzo.bodini@prabo.org>
2026-02-26 19:13:31 +0100

    feat(cover_art): state design change

--- src/tui/cover_art.rs
+++ src/tui/cover_art.rs
@@ -1,65 +1,95 @@
 use anyhow::anyhow;
+use log::{debug, info};
 use ratatui::{layout::Rect, Frame};
-use ratatui_image::{picker::Picker, protocol::StatefulProtocol, Resize, StatefulImage};
+use ratatui_image::{
+  picker::{Picker, ProtocolType},
+  protocol::StatefulProtocol,
+  Resize, StatefulImage,
+};
 use rspotify::model::Image;
 use std::sync::Mutex;
 use tokio::io::AsyncWriteExt;
 
 pub struct CoverArt {
-  pub state: Mutex<Option<StatefulProtocol>>,
-  url: Option<String>,
+  pub state: Mutex<Option<CoverArtState>>,
+  picker: Picker,
+}
+
+pub struct CoverArtState {
+  url: String,
+  image: StatefulProtocol,
+}
+
+impl CoverArtState {
+  fn new(url: String, image: StatefulProtocol) -> Self {
+    Self { url, image }
+  }
 }
 
 impl CoverArt {
   pub fn new() -> Self {
+    let picker = Picker::from_query_stdio().unwrap();
+
+    info!("cover art rendered detected a {:?} backend", picker.protocol_type());
     Self {
+      picker,
       state: Mutex::new(None),
-      url: None,
     }
   }
 
-  pub async fn refresh(&mut self, image: &Image) -> anyhow::Result<()> {
-    if self.url.as_ref() == Some(&image.url) {
-      // eprintln!("skipping image refresh: already exists");
-      assert!(self.state.lock().unwrap().is_some());
-      return Ok(());
+  pub fn full_image_support(&self) -> bool {
+    match self.picker.protocol_type() {
+      ProtocolType::Kitty | ProtocolType::Iterm2 => true,
+      ProtocolType::Sixel | ProtocolType::Halfblocks => false,
     }
+  }
 
-    let res = reqwest::get(&image.url).await;
-    let res = match res {
-      Ok(r) => r.error_for_status(),
-      Err(e) => Result::Err(e),
-    };
-    let file = match res {
-      Ok(res) => {
-        let mut file = match res.content_length() {
-          Some(s) => Vec::with_capacity(s as usize),
-          None => Vec::new(),
-        };
-        file
-          .write_all(&res.bytes().await.unwrap())
-          .await
-          .expect("error while downloading album art");
-        file
-      }
-      Err(e) => return Err(anyhow!(e)),
-    };
+  pub fn get_url(&self) -> Option<String> {
+    self.state.lock().unwrap().as_ref().map(|s| s.url.clone())
+  }
 
-    let picker = Picker::from_query_stdio().unwrap();
-    let image_protocol = picker.new_resize_protocol(image::load_from_memory(&file).unwrap());
-    // let ty = match image_protocol.protocol_type() {
-    //   StatefulProtocolType::Kitty(_) => "kitty",
-    //   StatefulProtocolType::Sixel(_) => "sixel",
-    //   StatefulProtocolType::ITerm2(_) => "iterm2",
-    //   StatefulProtocolType::Halfblocks(_) => "halfblocks",
-    // };
-    // eprintln!("USING IMAGE PROTOCOL TYPE: {}", ty);
-
-    self.url = Some(image.url.to_string());
+  pub fn set_state(&self, state: CoverArtState) {
     let mut lock = self.state.lock().unwrap();
-    *lock = Some(image_protocol);
+    *lock = Some(state);
+  }
 
-    // eprintln!("got new image: url={}, size={}", image.url, file.len());
+  pub async fn refresh(&self, image: &Image) -> anyhow::Result<()> {
+    if self.get_url().as_ref() != Some(&image.url) {
+      info!("getting new cover art image...");
+
+      let res = match reqwest::get(&image.url).await {
+        Ok(r) => r.error_for_status(),
+        Err(e) => Result::Err(e),
+      };
+
+      let file = match res {
+        Ok(res) => {
+          // Allocate Vec "file" with capacity if content_length is provided
+          let mut file = match res.content_length() {
+            Some(s) => Vec::with_capacity(s as usize),
+            None => Vec::new(),
+          };
+
+          file
+            .write_all(&res.bytes().await.unwrap())
+            .await
+            .expect("error while downloading album art");
+
+          debug!("finished reading response: {} bytes", file.len());
+          file
+        }
+        Err(e) => return Err(anyhow!(e)),
+      };
+
+      let image_protocol = self
+        .picker
+        .new_resize_protocol(image::load_from_memory(&file).unwrap());
+
+      self.set_state(CoverArtState::new(image.url.clone(), image_protocol));
+      info!("got new cover art: {}", image.url);
+    } else {
+      debug!("skipping image refresh: cover art already downloaded");
+    }
 
     Ok(())
   }
@@ -71,7 +101,11 @@ impl CoverArt {
   pub fn render(&self, f: &mut Frame, area: Rect) {
     let mut lock = self.state.lock().unwrap();
     if let Some(sp) = lock.as_mut() {
-      f.render_stateful_widget(StatefulImage::new().resize(Resize::Fit(None)), area, sp);
+      f.render_stateful_widget(
+        StatefulImage::new().resize(Resize::Fit(None)),
+        area,
+        &mut sp.image,
+      );
     }
   }
 }
